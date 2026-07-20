9694d27e453dd7c13e9027504662e9ca4d5d0f3e
sandih77 <sandihrandrianarijaona@gmail.com>
2026-07-20 15:20:22 +0300

    adding commission

--- base.sql
+++ base.sql
@@ -65,11 +65,11 @@ VALUES
     ('retrait'),
     ('transfert');
 
-INSERT INTO operateur (nom)
+INSERT INTO operateur (nom, commission)
 VALUES
-    ('telma'),
-    ('orange'),
-    ('airtel');
+    ('yas', 0),
+    ('orange', 5),
+    ('airtel', 7);
 
 INSERT INTO prefixes (prefixe, operateur_id)
 VALUES
