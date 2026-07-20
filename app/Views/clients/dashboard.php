84c4d692aeab762fbd2b4fd88307485037ac1690
sandih77 <sandihrandrianarijaona@gmail.com>
2026-07-20 10:34:27 +0300

    petit dashboard client

--- app/Views/clients/dashboard.php
+++ app/Views/clients/dashboard.php
@@ -4,5 +4,11 @@
 </head>
 <body>
     <h1>Bienvenue <?= esc($client->nom); ?></h1>
+    <ul>
+        <li><a href="<?= site_url('clients/depot')?>">Faire un dépôt</a></li>
+        <li><a href="<?= site_url('clients/retrait')?>">Faire un retrait</a></li>
+        <li><a href="<?= site_url('clients/transfert')?>">Faire un transfert</a></li>
+        <li><a href="<?= site_url('clients/logout')?>">Se déconnecter</a></li>
+    </ul>
 </body>
 </html>
